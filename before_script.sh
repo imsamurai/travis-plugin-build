bad1fbab2f538be515fa7e1cc09d5a5b6fa5f99d
imsamurai <imsamurai@imsamurai-2>
2014-12-09 15:05:38 +0200

    changes for codestyle

--- before_script.sh
+++ before_script.sh
@@ -5,7 +5,7 @@ if [ "$PHPDOC" = 1 ]; then
 	sudo apt-get -qq install graphviz;
 fi;
 
-git clone https://github.com/FriendsOfCake/travis.git --depth 1 ../travis;
+git clone https://github.com/imsamurai/travis.git --depth 1 ../travis;
 ../travis/before_script.sh;
 if [ "$PHPCS" != 1 ]; then
 	echo "<?php
@@ -14,11 +14,4 @@ if [ "$PHPCS" != 1 ]; then
 		require_once dirname(dirname(dirname(__FILE__))) . '/lib/Cake/Console/ShellDispatcher.php';
 		return ShellDispatcher::run(\$argv);
 	" > ../cakephp/app/Console/cake.php;
-fi;
-if [ "$PHPCS" = 1 ]; then
-	pear uninstall PHP_CodeSniffer;
-	pear install PHP_CodeSniffer-1.5.5;
-	phpenv rehash;
-	rm -rf ~/.phpenv/versions/$(phpenv version-name)/pear/PHP/CodeSniffer/Standards/CakePHP;
-	git clone https://github.com/imsamurai/cakephp-codesniffer.git --depth 1 ~/.phpenv/versions/$(phpenv version-name)/pear/PHP/CodeSniffer/Standards/CakePHP;
 fi;
