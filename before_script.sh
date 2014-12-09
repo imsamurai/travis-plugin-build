d05feb6e4658c7d591686d6efa4acf470e0b24f8
imsamurai <imsamurai@imsamurai-2>
2014-12-09 14:40:07 +0200

    fix phpcs version

--- before_script.sh
+++ before_script.sh
@@ -18,6 +18,7 @@ fi;
 if [ "$PHPCS" = 1 ]; then
 	pear uninstall PHP_CodeSniffer;
 	pear install PHP_CodeSniffer-1.5.5;
+	phpenv rehash;
 	rm -rf ~/.phpenv/versions/$(phpenv version-name)/pear/PHP/CodeSniffer/Standards/CakePHP;
 	git clone https://github.com/imsamurai/cakephp-codesniffer.git --depth 1 ~/.phpenv/versions/$(phpenv version-name)/pear/PHP/CodeSniffer/Standards/CakePHP;
 fi;
